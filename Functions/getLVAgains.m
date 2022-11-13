3c8fd7a10f403ee1bb08c312c791d9187a76ab80
stephensjake72 <45493756+stephensjake72@users.noreply.github.com>
2022-11-13 11:59:03 -0500

    Add files via upload

--- Functions/getLVAgains.m
+++ Functions/getLVAgains.m
@@ -1,4 +1,4 @@
-function [LVAgains, R2] = getLVAgains(data, parameters, opt)
+function fit = getLVAgains(data, parameters, opt)
 % constraints
 init = parameters(1, :);
 lower = parameters(2, :);
@@ -8,7 +8,7 @@ if strcmp(opt, 'MTU')
     L = data.Lmt;
     V = data.vmt;
     A = data.amt;
-elseif strcmp(opt, 'FAS')
+elseif strcmp(opt, 'Fas')
     L = data.Lf;
     V = data.vf;
     A = data.af;
@@ -17,18 +17,51 @@ time = data.time;
 spiketimes = data.spiketimes;
 IFR = data.IFR;
 
-% interpolate to spiketimes
-type = 'linear';
-L_st = interp1(time, L, spiketimes, type);
-V_st = interp1(time, V, spiketimes, type);
-A_st = interp1(time, A, spiketimes, type);
-
 % cost
-cost = @(gains) lva_cost(L_st, V_st, A_st, IFR, gains);
+cost = @(gains) lva_cost(L, V, A, time, spiketimes, IFR, gains);
 
 % run optimization
 options = optimoptions('fmincon', 'Display', 'off');
-[LVAgains, SSR] = fmincon(cost, init, [], [], [], [], lower, upper, [], options);
+[LVAgains, ~] = fmincon(cost, init, [], [], [], [], lower, upper, [], options);
 
-SST = sum((IFR - mean(IFR)).^2);
-R2 = 1 - SSR/SST;
+% time series fit data
+fit.L = L;
+fit.V = V;
+fit.A = A;
+% constants
+fit.kL = LVAgains(1);
+fit.kV = LVAgains(2);
+fit.kA = LVAgains(3);
+fit.bL = LVAgains(4);
+fit.bV = LVAgains(5);
+fit.bA = LVAgains(6);
+fit.lambda = LVAgains(7);
+% currents
+rL = fit.kL*(fit.L + fit.bL);
+rV = fit.kV*(fit.V + fit.bV);
+rA = fit.kA*(fit.A + fit.bA);
+rL(rL < 0) = 0; % rectify
+rV(rV < 0) = 0;
+rA(rA < 0) = 0;
+rT = rL + rV + rA;
+% occlusion
+fit.Locclusion = rL./rT;
+fit.Vocclusion = rV./rT;
+fit.Aocclusion = rA./rT;
+% components
+fit.Lcomp = rL.*fit.Locclusion;
+fit.Vcomp = rV.*fit.Vocclusion;
+fit.Acomp = rA.*fit.Aocclusion;
+% predictor
+fit.predictor = fit.Lcomp + fit.Vcomp + fit.Acomp;
+% error metrics
+p = interp1(time + fit.lambda, fit.predictor, spiketimes);
+C = corrcoef(p, IFR);
+fit.R = C(2, 1);
+fit.R2 = C(2, 1)^2;
+fit.resid = IFR - p;
+n = length(IFR);
+k = length(LVAgains(LVAgains ~=0));
+fit.R2adj = 1 - (1 - fit.R2)*(n - 1)/(n - k - 1);
+fit.VAF = 1-(var(IFR - p)/var(IFR));
+
